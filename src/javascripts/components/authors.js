f7700797ab8690a21fe0b1f4a1c5eb701bb8e46e
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2021-08-28 17:48:02 -0500

    added buttons to authors and books

--- src/javascripts/components/authors.js
+++ src/javascripts/components/authors.js
@@ -10,6 +10,10 @@ const showAuthors = (array) => {
       <div class="card-body">
         <h5 class="card-title">${item.first_name} ${item.last_name}</h5>
         <h6 class="card-subtitle mb-2 text-muted">${item.email}</h6>
+        <hr>
+        <i class="btn btn-success fas fa-eye" id="view-author-btn--${item.firebaseKey}"></i>
+        <i class="fas fa-edit btn btn-info" id="update-author--${item.firebaseKey}"></i>
+        <i class="btn btn-danger fas fa-trash-alt" id="delete-author--${item.firebaseKey}"></i>
       </div>
     </div>
     `;
