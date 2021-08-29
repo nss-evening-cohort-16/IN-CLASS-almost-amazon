53b2aa7904da1167f059ad379e95bfc643dc99ff
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2021-08-28 21:23:13 -0500

    add author details and style to view book details

--- src/javascripts/components/books.js
+++ src/javascripts/components/books.js
@@ -11,7 +11,7 @@ const showBooks = (array) => {
         <img class="card-img-top" src=${item.image} alt=${item.title} style="height: 400px;">
         <div class="card-body" style="height: 180px;">
           <h5 class="card-title">${item.title}</h5>
-            <p class="card-text bold">${item.sale ? `<span class="badge badge-info sale-badge"><i class="fa fa-bell" aria-hidden="true"></i> Sale</span> $${item.price}` : `$${item.price}`}</p>
+            <p class="card-text bold">${item.sale ? `<span class="badge bg-info"><i class="fa fa-bell" aria-hidden="true"></i> Sale</span> $${item.price}` : `$${item.price}`}</p>
             <hr>
             <i class="btn btn-success fas fa-eye" id="view-book-btn--${item.firebaseKey}"></i>
             <i id="edit-book-btn--${item.firebaseKey}" class="fas fa-edit btn btn-info"></i>
